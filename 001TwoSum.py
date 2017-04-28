6b2bf53c0177d11b6ada98db5b0cdee7aae93ed8
Steve <1211037294@qq.com>
2017-04-28 18:09:04 +0800

    1. Two Sum

---
+++ 001TwoSum.py
@@ -0,0 +1,18 @@
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        if len(nums) < 2:
+            return False
+
+        dictResult = {}
+
+        for i in range(len(nums)):
+            pass
+
+
+
+if __name__ == '__main__':
+    nums = [2, 7, 11, 15]
+    target = 9
+
+    Solution().twoSum(nums, target)
